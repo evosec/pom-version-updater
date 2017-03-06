24c55dc133573b879cfe0ba6c86b52bf0e099346
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2017-03-06 10:08:13 +0100

    Rename class

--- src/main/java/de/evosec/pomversionupdater/PomVersionUpdaterApplication.java
+++ src/main/java/de/evosec/pomversionupdater/PomVersionUpdaterApplication.java
@@ -25,10 +25,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication implements ApplicationRunner {
+public class PomVersionUpdaterApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(PomVersionUpdaterApplication.class, args);
 	}
 
 	@Override
