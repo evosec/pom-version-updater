50cc85ea484678bdc80c9eee083813a38a3fe946
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2017-03-06 10:30:25 +0100

    Multiple refactorings:
    Make sure Git is closed
    Use new Artifact class design

--- src/main/java/de/evosec/pomversionupdater/PomVersionUpdaterApplication.java
+++ src/main/java/de/evosec/pomversionupdater/PomVersionUpdaterApplication.java
@@ -1,14 +1,15 @@
 package de.evosec.pomversionupdater;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.nio.file.Path;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Constants;
@@ -34,65 +35,83 @@ public class PomVersionUpdaterApplication implements ApplicationRunner {
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		Path pom = Paths.get("pom.xml").toAbsolutePath();
-		Git git = Git.open(pom.getParent().toFile());
+		try (Git git = Git.open(pom.getParent().toFile())) {
 
-		git.getRepository().getRefDatabase().refresh();
-		IndexDiff diffIndex = new IndexDiff(git.getRepository(), Constants.HEAD, new FileTreeIterator(git.getRepository()));
-		if (diffIndex.diff()) {
-			throw new Exception("The working tree is not clean");
+			git.getRepository().getRefDatabase().refresh();
+			IndexDiff diffIndex = new IndexDiff(git.getRepository(),
+			    Constants.HEAD, new FileTreeIterator(git.getRepository()));
+			if (diffIndex.diff()) {
+				throw new Exception("The working tree is not clean");
+			}
+
+			Artifact beforeParent =
+			        selectArtifactsFromPom(pom, "project > parent").get(0);
+			if (beforeParent != null && beforeParent.getVersion() != null) {
+				// TODO: log output and check return code
+				new ProcessBuilder("mvn", "versions:update-parent",
+				    "-DgenerateBackupPoms=false").start().waitFor();
+				Artifact afterParent =
+				        selectArtifactsFromPom(pom, "project > parent").get(0);
+				commitIfNecessary(git, beforeParent, afterParent);
+			}
+
+			processDependencies(pom, git,
+			    "project > dependencyManagement > dependencies > dependency");
+			processDependencies(pom, git,
+			    "project > dependencies > dependency");
 		}
-
-		Artifact beforeParent = selectArtifactsFromPom(pom, "project > parent").get(0);
-		if (beforeParent != null && beforeParent.getVersion() != null) {
-			// TODO: log output and check return code
-			new ProcessBuilder("mvn", "versions:update-parent", "-DgenerateBackupPoms=false").start().waitFor();
-			Artifact afterParent = selectArtifactsFromPom(pom, "project > parent").get(0);
-			commitIfNecessary(git, beforeParent, afterParent);
-		}
-
-		processDependencies(pom, git, "project > dependencyManagement > dependencies > dependency");
-		processDependencies(pom, git, "project > dependencies > dependency");
 	}
 
-	private void processDependencies(Path pom, Git git, String selector) throws Exception {
+	private void processDependencies(Path pom, Git git, String selector)
+	        throws Exception {
 		List<Artifact> dependencies = selectArtifactsFromPom(pom, selector);
-		for (Artifact dependency : dependencies.stream().filter(a -> a.getVersion() != null).collect(Collectors.toList())) {
-			String include = String.format("%s:%s", dependency.getGroupId(), dependency.getArtifactId());
-			new ProcessBuilder("mvn", "versions:use-latest-versions", "-DgenerateBackupPoms=false", "-Dinclude=" + include).start().waitFor();
-			Artifact afterDependency = selectArtifactsFromPom(pom, selector).stream().filter(a -> a.equals(dependency)).findAny().get();
+		for (Artifact dependency : dependencies.stream()
+		    .filter(a -> a.getVersion() != null).collect(Collectors.toList())) {
+			String include = String.format("%s:%s", dependency.getGroupId(),
+			    dependency.getArtifactId());
+			new ProcessBuilder("mvn", "versions:use-latest-versions",
+			    "-DgenerateBackupPoms=false", "-Dinclude=" + include).start()
+			        .waitFor();
+			Artifact afterDependency = selectArtifactsFromPom(pom, selector)
+			    .stream().filter(a -> a.equals(dependency)).findAny().get();
 			commitIfNecessary(git, dependency, afterDependency);
 		}
 	}
 
-	private void commitIfNecessary(Git git, Artifact before, Artifact after) throws Exception {
+	private void commitIfNecessary(Git git, Artifact before, Artifact after)
+	        throws Exception {
 		if (!after.getVersion().equals(before.getVersion())) {
-			String message = String.format("%s:%s: %s -> %s", after.getGroupId(), after.getArtifactId(), before.getVersion(), after.getVersion());
+			String message = String.format("%s:%s: %s -> %s",
+			    after.getGroupId(), after.getArtifactId(), before.getVersion(),
+			    after.getVersion());
 			git.add().addFilepattern("pom.xml").call();
 			git.commit().setAllowEmpty(false).setMessage(message).call();
 		}
 	}
 
-	private List<Artifact> selectArtifactsFromPom(Path pom, String selector) throws IOException {
+	private List<Artifact> selectArtifactsFromPom(Path pom, String selector)
+	        throws IOException {
 		List<Artifact> artifacts = new ArrayList<>();
-		try(InputStream inputStream = Files.newInputStream(pom)) {
-			Document document = Jsoup.parse(inputStream, StandardCharsets.UTF_8.name(), "", Parser.xmlParser());
+		try (InputStream inputStream = Files.newInputStream(pom)) {
+			Document document = Jsoup.parse(inputStream, UTF_8.name(), "",
+			    Parser.xmlParser());
 			for (Element element : document.select(selector)) {
-				Artifact.ArtifactBuilder builder = new Artifact.ArtifactBuilder();
-				builder.groupId(element.select("groupId").first().text());
-				builder.artifactId(element.select("artifactId").first().text());
+				Artifact artifact =
+				        new Artifact(element.select("groupId").first().text(),
+				            element.select("artifactId").first().text());
 				Elements versionSelect = element.select("version");
 				if (!versionSelect.isEmpty()) {
-					builder.version(versionSelect.first().text());
+					artifact.setVersion(versionSelect.first().text());
 				}
 				Elements classifierSelect = element.select("classifier");
 				if (!classifierSelect.isEmpty()) {
-					builder.classifier(classifierSelect.first().text());
+					artifact.setClassifier(classifierSelect.first().text());
 				}
 				Elements typeSelect = element.select("type");
 				if (!typeSelect.isEmpty()) {
-					builder.type(typeSelect.first().text());
+					artifact.setType(typeSelect.first().text());
 				}
-				artifacts.add(builder.build());
+				artifacts.add(artifact);
 			}
 		}
 		return artifacts;
