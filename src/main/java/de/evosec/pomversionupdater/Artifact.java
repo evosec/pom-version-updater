e01a384e177fe8d878dacd80ba13dec02cc66022
Kay-Uwe Janssen <k.janssen@herbrand.de>
2025-07-30 10:22:02 +0200

    Artifact: simplify/modernize equals & hashCode

--- src/main/java/de/evosec/pomversionupdater/Artifact.java
+++ src/main/java/de/evosec/pomversionupdater/Artifact.java
@@ -48,59 +48,20 @@ public class Artifact {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ (artifactId == null ? 0 : artifactId.hashCode());
-		result = prime * result
-				+ (classifier == null ? 0 : classifier.hashCode());
-		result = prime * result + (groupId == null ? 0 : groupId.hashCode());
-		result = prime * result + (type == null ? 0 : type.hashCode());
-		return result;
+	public boolean equals(Object o) {
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Artifact artifact = (Artifact) o;
+		return Objects.equals(groupId, artifact.groupId)
+				&& Objects.equals(artifactId, artifact.artifactId)
+				&& Objects.equals(type, artifact.type)
+				&& Objects.equals(classifier, artifact.classifier);
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		Artifact other = (Artifact) obj;
-		if (artifactId == null) {
-			if (other.artifactId != null) {
-				return false;
-			}
-		} else if (!artifactId.equals(other.artifactId)) {
-			return false;
-		}
-		if (classifier == null) {
-			if (other.classifier != null) {
-				return false;
-			}
-		} else if (!classifier.equals(other.classifier)) {
-			return false;
-		}
-		if (groupId == null) {
-			if (other.groupId != null) {
-				return false;
-			}
-		} else if (!groupId.equals(other.groupId)) {
-			return false;
-		}
-		if (type == null) {
-			if (other.type != null) {
-				return false;
-			}
-		} else if (!type.equals(other.type)) {
-			return false;
-		}
-		return true;
+	public int hashCode() {
+		return Objects.hash(groupId, artifactId, type, classifier);
 	}
 
 	@Override
